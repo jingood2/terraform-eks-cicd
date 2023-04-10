6432f4044bd44555c5b8cc243819adb7606991e1
jingood2 <jingood2@gmail.com>
2023-04-10 14:34:52 +0900

    upsaved file upload

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,107 +1,19 @@
-# VPC Output Values
-
-# VPC ID
-output "vpc_id" {
-  description = "The ID of the VPC"
-  value       = module.vpc.vpc_id
+output "eks_cluster_id" {
+  description = "The name of the EKS cluster."
+  value       = module.eks_blueprints.eks_cluster_id
 }
 
-output "vpc_arn" {
-  description = "The ARN of the VPC"
-  value       = module.vpc.vpc_arn
+output "configure_kubectl" {
+  description = "Configure kubectl: make sure you're logged in with the correct AWS profile and run the following command to update your kubeconfig"
+  value       = module.eks_blueprints.configure_kubectl
 }
 
-# VPC CIDR blocks
-output "vpc_cidr_block" {
-  description = "The CIDR block of the VPC"
-  value       = module.vpc.vpc_cidr_block
+output "eks_cluster_endpoint" {
+  description = "The endpoint of the EKS cluster."
+  value       = module.eks_blueprints.eks_cluster_endpoint
 }
 
-# VPC Private Subnets
-output "private_subnets" {
-  description = "List of IDs of private subnets"
-  value       = module.vpc.private_subnets
-}
-
-output "private_subnet_arns" {
-  description = "List of ARNs of private subnets"
-  value       = module.vpc.private_subnet_arns
-}
-
-output "private_subnets_cidr_blocks" {
-  description = "List of cidr_blocks of private subnets"
-  value       = module.vpc.private_subnets_cidr_blocks
-}
-
-# VPC Public Subnets
-output "public_subnets" {
-  description = "List of IDs of public subnets"
-  value       = module.vpc.public_subnets
-}
-
-output "public_subnet_arns" {
-  description = "List of ARNs of public subnets"
-  value       = module.vpc.public_subnet_arns
-}
-
-output "public_subnets_cidr_blocks" {
-  description = "List of cidr_blocks of public subnets"
-  value       = module.vpc.public_subnets_cidr_blocks
-}
-
-output "database_subnets" {
-  description = "List of IDs of database subnets"
-  value       = module.vpc.database_subnets
-}
-
-output "database_subnet_arns" {
-  description = "List of ARNs of database subnets"
-  value       = module.vpc.database_subnet_arns
-}
-
-output "database_subnets_cidr_blocks" {
-  description = "List of cidr_blocks of database subnets"
-  value       = module.vpc.database_subnets_cidr_blocks
-}
-
-# VPC NAT gateway Public IP
-output "nat_public_ips" {
-  description = "List of public Elastic IPs created for AWS NAT Gateway"
-  value       = module.vpc.nat_public_ips
-}
-
-# VPC AZs
-output "azs" {
-  description = "A list of availability zones spefified as argument to this module"
-  value       = module.vpc.azs
-}
-
-output "default_security_group_id" {
-  description = "The ID of the security group created by default on VPC creation"
-  value       = module.vpc.default_security_group_id
-}
-
-output "default_network_acl_id" {
-  description = "The ID of the default network ACL"
-  value       = module.vpc.default_network_acl_id
-}
-
-output "default_route_table_id" {
-  description = "The ID of the default route table"
-  value       = module.vpc.default_route_table_id
-}
-
-output "vpc_enable_dns_support" {
-  description = "Whether or not the VPC has DNS support"
-  value       = module.vpc.vpc_enable_dns_support
-}
-
-output "vpc_enable_dns_hostnames" {
-  description = "Whether or not the VPC has DNS hostname support"
-  value       = module.vpc.vpc_enable_dns_hostnames
-}
-
-output "vpc_secondary_cidr_blocks" {
-  description = "List of secondary CIDR blocks of the VPC"
-  value       = module.vpc.vpc_secondary_cidr_blocks
+output "eks_cluster_certificate_authority_data" {
+  description = "eks_cluster_certificate_authority_data"
+  value       = module.eks_blueprints.eks_cluster_certificate_authority_data
 }
