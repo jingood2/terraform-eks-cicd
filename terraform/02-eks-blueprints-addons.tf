719ff7aa2b61ff6e42d8f9fcfea18ffeef8108b9
jingood2 <jingood2@gmail.com>
2023-04-12 10:09:15 +0900

    Wait on the node group(s) before provisioning addo

--- terraform/02-eks-blueprints-addons.tf
+++ terraform/02-eks-blueprints-addons.tf
@@ -9,6 +9,9 @@ module "kubernetes_addons" {
   eks_oidc_provider     = module.eks_blueprints.oidc_provider
   eks_cluster_version   = module.eks_blueprints.eks_cluster_version
 
+  # Wait on the node group(s) before provisioning addons
+  data_plane_wait_arn = join(",", [for group in module.eks_blueprints.eks_managed_node_groups : group.node_group_arn])
+
   #-----------------AWS Managed EKS Add-ons----------------------
   enable_amazon_eks_aws_ebs_csi_driver = true
 
