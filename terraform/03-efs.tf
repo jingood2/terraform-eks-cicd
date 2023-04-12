15ed36f2916ce990eaa9824ccaa6395c08dca7d2
jingood2 <jingood2@gmail.com>
2023-04-12 17:59:57 +0900

    fix data aws_subnets error

--- terraform/03-efs.tf
+++ terraform/03-efs.tf
@@ -1,16 +1,3 @@
-
-data "aws_subnets" "private" {
-  filter {
-    name   = "tag:${var.vpc_tag_key}"
-    values = ["${local.tag_val_private_subnet}*"]
-  }
-}
-
-# Declare the data source
-data "aws_availability_zones" "available" {
-  state = "available"
-}
-
 module "efs" {
   source  = "terraform-aws-modules/efs/aws"
   version = "1.1.1"
@@ -24,7 +11,7 @@ module "efs" {
   throughput_mode                 = "provisioned"
 
   # Mount targets / security group
-  mount_targets              = { for k, v in zipmap(["us-east-1a", "us-east-1c"], data.aws_subnets.private.id) : k => { subnet_id = v } }
+  mount_targets              = { for k, v in zipmap(["us-east-1a", "us-east-1c"], data.aws_subnets.private.ids) : k => { subnet_id = v } }
 
   security_group_name = "${local.prefix_name}-efs-sg"
   security_group_description = "${local.prefix_name} EFS security group"
