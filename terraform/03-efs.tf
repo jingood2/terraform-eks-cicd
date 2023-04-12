efd7eb05fd04eba5106c5cce940121189ed76b63
jingood2 <jingood2@gmail.com>
2023-04-12 18:03:21 +0900

    fix vpc

--- terraform/03-efs.tf
+++ terraform/03-efs.tf
@@ -15,7 +15,7 @@ module "efs" {
 
   security_group_name = "${local.prefix_name}-efs-sg"
   security_group_description = "${local.prefix_name} EFS security group"
-  security_group_vpc_id      = module.vpc.vpc_id
+  security_group_vpc_id      = data.aws_vpc.vpc.id
   security_group_rules = {
     #vpc = {
     #  # relying on the defaults provdied for EFS/NFS (2049/TCP + ingress)
